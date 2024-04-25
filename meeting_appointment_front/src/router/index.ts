da36e9018cc5ae1f64033476511883e25d80b3c7
unknown <ying.zhou@YTX.COM>
2024-04-25 11:02:20 +0800

    Add -> 登录页面

--- meeting_appointment_front/src/router/index.ts
+++ meeting_appointment_front/src/router/index.ts
@@ -1,17 +1,21 @@
-import component from 'element-plus/es/components/tree-select/src/tree-select-option.mjs';
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-    {
-        path: '/',
-        name: "layout",
-        component: ()=>import("@/layout/index.vue"),
-    }
+  {
+    path: '/',
+    name: "layout",
+    component: () => import("@/layout/index.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/login/login.vue"),
+  }
 ];
 const router = createRouter({
-    history: createWebHistory(),
-    linkActiveClass: 'active',
-    routes,
-  });
+  history: createWebHistory(),
+  linkActiveClass: 'active',
+  routes,
+});
 
 export default router
