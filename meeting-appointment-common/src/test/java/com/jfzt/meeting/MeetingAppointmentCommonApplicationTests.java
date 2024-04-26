03b892a7dd36472e66794c14d3ef7c5e988959dc
zilong.deng <zilong.deng@9fzt.com.cn>
2024-04-26 09:12:24 +0800

    refactor [project] : 项目结构调整

---
+++ meeting-appointment-common/src/test/java/com/jfzt/meeting/MeetingAppointmentCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfzt.meeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingAppointmentCommonApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
---
+++ meeting-appointment-modules/src/test/java/com/jfzt/modules/MeetingModulesTests.java
@@ -0,0 +1,13 @@
+package com.jfzt.modules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingModulesTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
