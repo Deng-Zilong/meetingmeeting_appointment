017d5ecfa7f20561358b469736f8e23e236ca9c1
ying.zhou <2212878840@qq.com>
2024-04-28 11:46:25 +0800

    Add -> 预约会议室时间与登陆页面

--- meeting-appointment-ui/src/views/login/login.vue
+++ meeting-appointment-ui/src/views/login/login.vue
@@ -1,45 +1,58 @@
 <template>
-  <div class="container">
-    <div class="leftBackground"></div>
-    <div class="rightLogin">
-      <div class="logo"></div>
-      <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules">
-        <!-- 域名 -->
-        <el-form-item prop="username">
-          <el-input v-model="ruleForm.username" placeholder="账号"/>
-        </el-form-item>
-        <!-- 密码 -->
-        <el-form-item prop="password">
-          <el-input v-model="ruleForm.password" type="password" placeholder="密码"/>
-        </el-form-item>
-        <!-- 验证码 -->
-        <el-form-item prop="captcha">
-          <el-row :gutter="20">
-            <el-col :span="12">
-              <el-input v-model="ruleForm.captcha"></el-input>
-            </el-col>
-            <el-col :span="12">
-              <div class="captcha">
-                <el-image :src="imgUrl" @click="changeCaptcha" draggable="false"/>
-              </div>
-            </el-col>
-          </el-row>
-        </el-form-item>
-        <!-- 登录 -->
-        <el-form-item>
-          <el-button type="primary" @click="submitForm(ruleFormRef)">
-            登录
-          </el-button>
-        </el-form-item>
-      </el-form>
+  <div class="login-container">
+    <div class="left-background"></div>
+    <div class="right-login">
+      <!-- 账号登录 -->
+      <div class="login-number" v-if="isShow">
+        <div class="logo"></div>
+        <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules">
+          <!-- 域名 -->
+          <el-form-item prop="username">
+            <el-input v-model="ruleForm.username" placeholder="账号" />
+          </el-form-item>
+          <!-- 密码 -->
+          <el-form-item prop="password">
+            <el-input v-model="ruleForm.password" type="password" placeholder="密码" />
+          </el-form-item>
+          <!-- 验证码 -->
+          <el-form-item prop="captcha">
+            <el-row :gutter="20">
+              <el-col :span="12"><el-input v-model="ruleForm.captcha"></el-input></el-col>
+              <el-col :span="12">
+                <div class="captcha">
+                  <el-image :src="imgUrl" @click="changeCaptcha" draggable="false" />
+                </div>
+              </el-col>
+            </el-row>
+          </el-form-item>
+          <!-- 登录 -->
+          <el-form-item>
+            <el-button type="primary" @click="submitForm(ruleFormRef)">
+              登录
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+
+      <!-- 扫码登录 -->
+      <div id="login-scan" v-else>
+      </div>
+
+      <el-button type="primary" @click="changeLogin">
+        点击{{ isShow == false ? '账号' : '扫码' }}登录
+      </el-button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {h, reactive, ref} from 'vue'
-import type {FormInstance, FormRules} from 'element-plus'
-import {ElMessage} from 'element-plus'
+import { nextTick, onMounted, reactive, ref } from 'vue'
+import type { FormInstance, FormRules } from 'element-plus'
+import { useRouter } from 'vue-router'
+import { h } from 'vue'
+import { ElMessage } from 'element-plus'
+import * as ww from '@wecom/jssdk'
+
 
 // const checkAge = (rule: any, value: any, callback: any) => {
 //   if (!value) {
@@ -80,7 +93,7 @@ import {ElMessage} from 'element-plus'
 // }
 
 const ruleFormRef = ref<FormInstance>()
-
+// 登录验证
 const ruleForm = reactive({
   username: '',
   password: '',
@@ -92,16 +105,16 @@ const rules = reactive<FormRules<typeof ruleForm>>({
   // password: [{ validator: validatePass, trigger: 'blur' }],
   // captcha: [{ validator: checkAge, trigger: 'blur' }],
   username: [
-    {required: true, message: "请输入域名", trigger: "blur"},
-    {min: 5, max: 18, message: "域名必须是5-18位字符", trigger: "blur"}
+    { required: true, message: "请输入域名", trigger: "blur" },
+    { min: 5, max: 18, message: "域名必须是5-18位字符", trigger: "blur" }
   ],
   password: [
-    {required: true, message: "请输入密码", trigger: "blur"},
-    {min: 6, max: 24, message: "密码必须是6-24位字符", trigger: "blur"},
+    { required: true, message: "请输入密码", trigger: "blur" },
+    { min: 6, max: 24, message: "密码必须是6-24位字符", trigger: "blur" },
   ],
   captcha: [
-    {required: true, message: "请输入验证码", trigger: "blur"},
-    {min: 5, max: 18, message: "验证码必须是4位字符", trigger: "blur"}
+    { required: true, message: "请输入验证码", trigger: "blur" },
+    { min: 4, max: 4, message: "验证码必须是4位字符", trigger: "blur" }
   ]
 })
 
@@ -111,56 +124,104 @@ const changeCaptcha = () => {
   imgUrl.value = ''
 }
 
-// 弹窗
+// 登录错误弹窗
 const openVn = () => {
   ElMessage({
-    icon: '',
-    message: h('div', {style: 'width: 550px; height: 100px; border-radius: 150px; font-size: 1.375rem'}, [
+    icon: 'none',
+    message: h('div',
+      { style: 'display: flex; align-items: center; justify-content: center; width: 26rem; height: 4.25rem; font-size: 1.375rem' }, [
       h('img', {
         src: '../src/assets/img/error.png',
         style: 'width: 4.375rem; height: 4.375rem;'
       }),
-      h('span', null, 'Message can be '),
-      h('i', {style: 'color: teal'}, 'VNode')
+      h('span', null, '域账号输入错误！')
     ]),
-    duration: 0
+    duration: 10000
   })
 }
 
-// 登录 
+// 账号切换
+const isShow = ref(false)
+const changeLogin = () => {
+  isShow.value = !isShow.value;
+  if (!isShow.value) {
+    nextTick(() => {
+      code();
+    })
+  }
+}
+// 账号登录 
+const router = useRouter()
 const submitForm = (formEl: FormInstance | undefined) => {
   if (!formEl) return
   formEl.validate((valid) => {
     if (valid) {
-      console.log('submit!')
+      router.push('/')
+      ElMessage.success('登陆成功')
     } else {
       openVn()
-      console.log('error submit!')
       return false
     }
   })
 }
 
-// const resetForm = (formEl: FormInstance | undefined) => {
-//   if (!formEl) return
-//   formEl.resetFields()
-// }
+// 扫码登录
+// onMounted(() => {
+//   const wwLogin = ww.createWWLoginPanel({
+//     el: '#qr_login',
+//     params: {
+//       appid: 'ww942086e6c44abc4b',
+//     agentid: '1000002',
+//      redirect_uri: 'ggssyy.cn/login/logininfo',
+//       state: 'WWLogin',
+//       // redirect_type: 'callback'
+//     },
+//     onCheckWeComLogin({ isWeComLogin }) {
+//       console.log(isWeComLogin)
+//     },
+//     onLoginSuccess({ code }) {
+//       console.log({ code })
+//     },
+//     onLoginFail(err) {
+//       console.log(err)
+//     }
+//   })
+// })
+
+
+onMounted(() => {
+  // changeCaptcha();
+  code();
+});
+const code = () => {
+  //@ts-ignore
+  new WwLogin({
+    "id": "login-scan",
+    appid: 'ww942086e6c44abc4b',
+    agentid: '1000002',
+    redirect_uri: 'ggssyy.cn/login/logininfo',
+    "state": Math.ceil(Math.random() * 1000),
+    "lang": "zh",
+  });
+}
+
+
 </script>
 
 <style scoped lang="scss">
-.container {
+.login-container {
   display: flex;
   width: 100vw;
   height: 100vh;
 
-  .leftBackground {
+  .left-background {
     width: 70vw;
     height: 100%;
     background: url(@/assets/img/login.png) no-repeat;
     background-size: 100% 100%;
   }
 
-  .rightLogin {
+  .right-login {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -168,22 +229,26 @@ const submitForm = (formEl: FormInstance | undefined) => {
     width: 30vw;
     height: 100%;
 
-    .logo {
-      width: 14.3125rem;
-      height: 4.6875rem;
-      margin-right: -4.5rem;
-      background: url(@/assets/img/logo.png) no-repeat;
-    }
+    // 账号登录
+    .login-number {
+      .logo {
+        width: 14.3125rem;
+        height: 4.6875rem;
+        margin-right: -4.5rem;
+        background: url(@/assets/img/logo.png) no-repeat;
+      }
 
-    .captcha {
-      .el-image {
+      .captcha {
+        .el-image {
+          width: 100%;
+        }
+      }
+
+      .el-button {
         width: 100%;
       }
     }
 
-    .el-button {
-      width: 100%;
-    }
   }
 }
 </style>
--- meeting-appointment-ui/src/views/meeting-state/meeting-state.vue
+++ meeting-appointment-ui/src/views/meeting-state/meeting-state.vue
@@ -1,3 +1,389 @@
 <template>
-    <div>会议室状态</div>
+  <!-- 会议室状态 -->
+  <div class="container meeting-container">
+    <header>
+      <el-divider direction="vertical" />会议室名称
+    </header>
+    <main>
+      <div class="meeting-left">
+        <div class="meeting-left-date">
+          <el-date-picker v-model="date" type="date" class="left-date" :disabled-date="disabledDate"
+            placeholder="选择日期" />
+        </div>
+        <div class="left-table">
+          <div class="table-title">会议预约时间选择</div>
+          <div class="table-main">
+            <div class="table-items" :class="color(item.state)" v-for="item in timeArr" @click.stop="selectTime(item)">
+              {{ item.time }}
+            </div>
+          </div>
+        </div>
+        <div class="left-state">
+          <div class="state-items">
+            <div class="state-items-color color-one"></div>
+            <div class="state-items-text">已过期</div>
+          </div>
+          <div class="state-items">
+            <div class="state-items-color color-two"></div>
+            <div class="state-items-text">已预定</div>
+          </div>
+          <div class="state-items">
+            <div class="state-items-color color-three"></div>
+            <div class="state-items-text">已选中</div>
+          </div>
+          <div class="state-items">
+            <div class="state-items-color color-four"></div>
+            <div class="state-items-text">可预约</div>
+          </div>
+        </div>
+      </div>
+      <div class="meeting-right">
+        <div class="right-title">{{ time }}</div>
+        <div class="right-table">
+          <div class="right-items" v-for="item in infoArr">
+            <img class="items-left" style="width: 40px;height: 40px;" :src="item.src" alt="">
+            <div class="items-right">
+              <div class="items-right-title">{{ item.title }}</div>
+              <div class="items-right-info">{{ item.info }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref, reactive, computed } from 'vue'
+import { useRouter } from 'vue-router';
+
+/* 会议日期选择 */
+const date = ref(new Date())
+// 禁止选择今日之前的日期
+const disabledDate = (date: any) => {
+  return date.getTime() < Date.now() - 8.64e7
+}
+
+// 显示当前时间点
+const time = ref(new Date().toLocaleTimeString().substring(0, 5))
+setInterval(() => {
+  time.value = new Date().toLocaleTimeString().substring(0, 5)
+}, 1000)
+
+
+// 会议时间点
+interface Time {
+  time: String,
+  state: Number
+}
+const timeArr = reactive<Time[]>([
+  {
+    time: '1714114711',
+    state: 4
+  }, {
+    time: '8:30',
+    state: 4
+  }, {
+    time: '9:00',
+    state: 4
+  }, {
+    time: '9:30',
+    state: 1
+  }, {
+    time: '8:00',
+    state: 1
+  },
+  {
+    time: '8:30',
+    state: 2
+  }, {
+    time: '8:00',
+    state: 3
+  },
+  {
+    time: '8:30',
+    state: 4
+  }, {
+    time: '8:00',
+    state: 4
+  },
+  {
+    time: '8:30',
+    state: 1
+  }, {
+    time: '8:00',
+    state: 4
+  }, {
+    time: '8:30',
+    state: 4
+  }, {
+    time: '9:00',
+    state: 4
+  }, {
+    time: '9:30',
+    state: 1
+  }, {
+    time: '8:00',
+    state: 1
+  },
+  {
+    time: '8:30',
+    state: 2
+  }, {
+    time: '8:00',
+    state: 3
+  },
+  {
+    time: '8:30',
+    state: 4
+  }, {
+    time: '8:00',
+    state: 4
+  },
+  {
+    time: '8:30',
+    state: 1
+  }, {
+    time: '8:00',
+    state: 0
+  }, {
+    time: '8:30',
+    state: 0
+  }, {
+    time: '9:00',
+    state: 0
+  }, {
+    time: '9:30',
+    state: 1
+  }, {
+    time: '8:00',
+    state: 1
+  },
+  {
+    time: '8:30',
+    state: 2
+  }, {
+    time: '8:00',
+    state: 3
+  },
+  {
+    time: '8:30',
+    state: 4
+  }, {
+    time: '8:00',
+    state: 0
+  },
+  {
+    time: '8:30',
+    state: 1
+  },
+])
+const color = computed(() => (state: any) => {
+  switch (state) {
+    case 1:
+      return 'color-one';
+    case 2:
+      return 'color-two';
+  }
+})
+
+// 选择时间段
+const routes = useRouter()
+const selectTime = (item: any) => {
+  if ([1, 2].includes(item.state)) {
+    return;
+  } else {
+    routes.push('/meeting-appoint')
+  }
+}
+
+// 会议室信息
+const infoArr = reactive([
+  {
+    src: '../src/assets/img/chamber.png',
+    title: '会议室名称',
+    info: '至和通宝'
+  },
+  {
+    src: '../src/assets/img/address.png',
+    title: '第3层',
+    info: '西北裙-3F-至和通宝'
+  },
+  {
+    src: '../src/assets/img/capacity.png',
+    title: '会议室容量',
+    info: '8人'
+  },
+  {
+    src: '../src/assets/img/device.png',
+    title: '设备',
+    info: '投屏TV'
+  }
+])
+
+</script>
+
+<style scoped lang="scss">
+.meeting-container {
+  // 公共样式 上传时删除
+  width: 101rem;
+  height: 45.9rem;
+  border-radius: 15px;
+  background-color: #ffffff;
+  box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.08);
+  // 公共样式结束
+
+  padding: 2.5rem 3.5rem;
+
+  // 预约时间模块颜色 && 状态样式颜色
+  .color-one {
+    background: #E2E2E2 !important;
+    cursor: not-allowed !important;
+  }
+
+  .color-two {
+    background: #FFD4B0 !important;
+    cursor: not-allowed !important;
+  }
+
+  header {
+    font-size: 1.5rem;
+
+    .el-divider {
+      height: 3.125rem;
+      border: .25rem solid #1273DB;
+      border-radius: .25rem;
+      margin-right: 1.5rem;
+    }
+  }
+
+  main {
+    display: flex;
+    justify-content: space-between;
+
+    .meeting-left {
+      width: 1080px;
+
+      .meeting-left-date {
+        display: flex;
+        justify-content: flex-end;
+        margin: .5rem 0;
+      }
+
+      .left-table {
+        .table-title {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 4.2rem;
+          background: #FFF;
+          border-radius: 15px 15px 0px 0px;
+          box-shadow: inset 0px 1px 8px 0px #DBE9F7;
+          color: #3E78F4;
+          font-size: 1.5rem;
+        }
+
+        .table-main {
+          display: flex;
+          flex-wrap: wrap;
+
+          .table-items {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 11rem;
+            height: 5rem;
+            margin: 0.125rem;
+            background: #FFF;
+            border-radius: 5px;
+            box-shadow: inset 0px 1px 8px 0px #DBE9F7;
+            cursor: pointer;
+
+            &:hover {
+              background-color: #1273DB;
+            }
+          }
+        }
+      }
+
+      .left-state {
+        display: flex;
+        justify-content: space-between;
+        margin: 1.25rem;
+
+        .state-items {
+          display: flex;
+          align-items: center;
+
+          // 状态公共样式
+          .state-items-color {
+            width: 3.125rem;
+            height: 3.125rem;
+            margin: 0 1.2rem;
+            border-radius: 5px;
+            box-shadow: 0px 3px 8px 0px rgba(0, 0, 0, 0.16);
+          }
+
+          // 状态单独样式
+          .color-three {
+            background: #417AF3 !important;
+          }
+
+          .state-items-text {
+            font-size: 1.6rem;
+            color: #636363;
+          }
+        }
+      }
+
+    }
+
+    .meeting-right {
+      width: 359px;
+      height: 38.75rem;
+      border-radius: 15px;
+      background: #FFFFFF;
+      box-shadow: inset 0px 1px 8px 0px #DBE9F7;
+
+      .right-title {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 80px;
+        color: #3E78F4;
+        font-size: 2.5rem;
+      }
+
+      .right-table {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        height: 33.75rem;
+        padding: 0 2.5rem;
+        border-radius: 0 0 15px 15px;
+        box-shadow: inset 0px 1px 8px 0px #DBE9F7;
+        font-size: 1.5rem;
+
+        .right-items {
+          display: flex;
+          align-items: center;
+
+          .items-left {
+            margin-right: 10px;
+          }
+
+          .items-right {
+            .items-right-title {
+              color: #3E78F4;
+            }
+
+            .items-right-info {
+              color: #585858;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
