e4923f0753c7ae94b8415c0410a37071e7cbc40d
chenyu.di <chenyu.di@9fzt.cn>
2024-05-16 19:24:04 +0800

    refactor [Interceptor，yml]重写JWT获取token

--- meeting-appointment-common/src/main/java/com/jfzt/meeting/config/TokenInterceptor.java
+++ meeting-appointment-common/src/main/java/com/jfzt/meeting/config/TokenInterceptor.java
@@ -1,11 +1,17 @@
 package com.jfzt.meeting.config;
 
 import com.jfzt.meeting.exception.ErrorCodeEnum;
+import com.jfzt.meeting.properties.JwtProperties;
+import com.jfzt.meeting.utils.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.io.IOException;
@@ -17,19 +23,38 @@ import java.io.IOException;
  */
 @Slf4j
 @Component
-public class TokenInterceptor implements HandlerInterceptor {
+public class  TokenInterceptor  implements HandlerInterceptor {
+
+    @Resource
+    private JwtProperties jwtProperties;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response,@NonNull Object handler) throws IOException {
-        // 从HTTP头信息中取得token
-        String token = request.getHeader("Authorization");
+    public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+                                                                  @NonNull Object handler) throws IOException {
+        response.addHeader("Access-Control-Expose-Headers","token");
+        //判断当前拦截到的是Controller的方法还是其他资源
+        if (!(handler instanceof HandlerMethod)) {
+            //当前拦截到的不是动态方法，直接放行
+            return true;
+        }
+        //放行OPTIONS请求
+//        if (HttpMethod.GET.toString().equals(request.getMethod())) {
+//            System.out.println("OPTIONS请求，放行");
+//            return true;
+//        }
+        //1、从请求头中获取令牌
+        String token = request.getHeader(jwtProperties.getUserTokenName());
 
-        // 如果token为空，则返回false，表示拦截请求
-        if (token == null || token.isEmpty()) {
-            response.sendError(500, ErrorCodeEnum.SERVICE_ERROR_A0230.getDescription());
+        //2、校验令牌
+        try {
+            log.info("jwt校验:{}", token);
+            Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
+            //3、通过，放行
+            return true;
+        } catch (Exception ex) {
+            //4、不通过，响应401状态码
+            response.setStatus(401);
             return false;
         }
-        // 如果token不为空，则继续处理请求
-        return true;
     }
 }
