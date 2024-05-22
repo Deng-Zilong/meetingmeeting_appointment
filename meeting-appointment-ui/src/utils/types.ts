371998c5265816bb1e5f26deae734727e3b5d356
zilong.deng <zilong.deng@9fzt.com.cn>
2024-05-22 10:25:10 +0800

    refactor [ui] 非创建人不可删除今日会议记录

--- meeting-appointment-ui/src/utils/types.ts
+++ meeting-appointment-ui/src/utils/types.ts
@@ -1,11 +1,11 @@
 /**
  * @description: 全局配置
- * 
+ *
  */
-import { reactive } from "vue";
+import {reactive} from "vue";
 
 export const meetingState = reactive([
-    {value: 0, label: '未开始'},
+    {value: 0, label: '已预约'},
     {value: 1, label: '进行中'},
     {value: 2, label: '已结束'},
     {value: 3, label: '已取消'},
