9a36b248cb00f428d3c0f0808ce2b71841cfff9d
mengmeng.kou <2256378537@qq.com>
2024-05-30 15:24:16 +0800

    perf: 优化群组接口地址

--- meeting-appointment-ui/src/request/api/group.ts
+++ meeting-appointment-ui/src/request/api/group.ts
@@ -1,27 +1,27 @@
 import http from "@/request/http";
 // 获取群组列表数据
 export const getMeetingGroupList = (data: {userId: string, pageNum?: number, pageSize?: number, }) => {
-    return http.get("/meeting/meeting-group/get-meeting-group-list", data)
+    return http.get("/meeting/meeting-group-list", data)
 }
 // 删除群组
 export const deleteMeetingGroup = (data: {id: number | string}) => {
-    return http.delete("/meeting/meeting-group/delete-meeting-group", data)
+    return http.delete("/meeting/meeting-group", data)
 }
 // 修改群组信息（群组名称和群组人员）
 export const updateMeetingGroup = (data: {id: string, groupName?: string, users?: []}) => {
-    return http.put("/meeting/meeting-group/update-meeting-group", data)
+    return http.put("/meeting/meeting-group", data)
 }
 // 添加群组
 export const addMeetingGroup = (data: {groupName: string, createdBy: string, userName: string, users: []}) => {
-    return http.post("/meeting/meeting-group/add-meeting-group", data)
+    return http.post("/meeting/meeting-group", data)
 }
 
 // 获取群组成员数据
 export const getGroupUserTree = () => {
-    return http.get("/meeting/group-user-tree/get-group-user-tree")
+    return http.get("/meeting/tree/group-user-tree")
 }
 
 // 根据群组成员名称模糊查询
 export const likeByName = (data: {name: string}) => {
-    return http.get("/meeting/group-user-tree/like-by-name", data)
+    return http.get("/meeting/tree/like-by-name", data)
 }
