027fd47660314ec7781c86f8781c0944eeee6843
zilong.deng <zilong.deng@9fzt.com.cn>
2024-05-30 10:52:24 +0800

    refactor [service,entity,controller,mapper] 添加会议纪要，会议纪要查询

---
+++ meeting-appointment-modules/src/main/java/com/jfzt/meeting/service/MeetingMinutesService.java
@@ -0,0 +1,26 @@
+package com.jfzt.meeting.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.jfzt.meeting.entity.MeetingMinutes;
+import com.jfzt.meeting.entity.MeetingMinutesVO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author zilong.deng
+ * @description 针对表【meeting_minutes(会议纪要)】的数据库操作Service
+ * @createDate 2024-05-29 18:04:16
+ */
+@Service
+public interface MeetingMinutesService extends IService<MeetingMinutes> {
+
+    /**
+     * @param meetingMinutes 会议记录id或用户id
+     * @return {@code List<MeetingMinutesVO>}
+     * @description 根据会议记录id或用户id查询会议纪要
+     */
+    List<MeetingMinutesVO> getMeetingMinutes (MeetingMinutes meetingMinutes);
+
+
+}
