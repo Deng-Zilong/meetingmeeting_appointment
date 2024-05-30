87f9a4cb53404be438aade52f3f5be3aa392d7d4
zhenxing.lu <zhenxing.lu@9fzt.com.cn>
2024-05-30 12:04:45 +0800

    refactor[] yml配置完善

--- meeting-appointment-modules/src/main/java/com/jfzt/meeting/service/impl/SysDepartmentUserServiceImpl.java
+++ meeting-appointment-modules/src/main/java/com/jfzt/meeting/service/impl/SysDepartmentUserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jfzt.meeting.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.jfzt.meeting.common.Result;
 import com.jfzt.meeting.entity.SysDepartment;
@@ -73,18 +74,15 @@ public class SysDepartmentUserServiceImpl extends ServiceImpl<SysDepartmentUserM
     @Override
     public WxCpUser findUserName(String accessToken, String code) throws WxErrorException {
         //获取用户user_ticket
-        String s ="0";
-        String tes = "errcode";
         HttpClientUtil httpClientUtil = new HttpClientUtil();
         HashMap<String, String> tokenCode = new HashMap<>(2);
         tokenCode.put("access_token", accessToken);
         tokenCode.put("code", code);
         String responseAll = httpClientUtil.doGet("https://qyapi.weixin.qq.com/cgi-bin/auth/getuserinfo", tokenCode);
-        JSONObject responseAllList = JSONObject.fromObject(responseAll);
-        if(!s.equals(responseAllList.getString(tes))){
-            log.error("请求企业微信失败");
-            throw new RRException(ErrorCodeEnum.SERVICE_ERROR_C00011);
+        if (1==1){
+            throw new RRException(responseAll);
         }
+        JSONObject responseAllList = JSONObject.fromObject(responseAll);
         String userid = responseAllList.getString("userid");
         //获取用户详细信息
         WxCpUserServiceImpl wxCpUserService = new WxCpUserServiceImpl(wxCpService);
@@ -95,6 +93,9 @@ public class SysDepartmentUserServiceImpl extends ServiceImpl<SysDepartmentUserM
     public Long findDepartment() throws WxErrorException {
         WxCpDepartmentServiceImpl wxCpDepartmentService = new WxCpDepartmentServiceImpl(wxCpService);
         List<WxCpDepart> listDepartmentList = wxCpDepartmentService.list(0L);
+        if (1==1){
+            throw  new RRException(listDepartmentList.toString());
+        }
         List<SysDepartment> sysDepartmentList = sysDepartmentMapper.selectList(null);
         if (sysDepartmentList.size() != 0){
             return (long) sysDepartmentList.size();
